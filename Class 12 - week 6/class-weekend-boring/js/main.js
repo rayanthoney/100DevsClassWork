5eff4d01478b480ca61bd2eb780cd7db9b4149f6
Ryan <ryanmahara@gmail.com>
2022-02-22 11:24:35 -0600

    class 12 class work

--- Class 12 - week 6/class-weekend-boring/js/main.js
+++ Class 12 - week 6/class-weekend-boring/js/main.js
@@ -1,10 +1,14 @@
-document.querySelector('#check').addEventListener('click', check)
+document.querySelector('#check').addEventListener('click', check);
 
 function check() {
-
-  const day = document.querySelector('#day').value
+  const day = document.querySelector('#day').value.toLowerCase();
 
   //Conditionals go here
-
-
+  if (day === 'tue' || day === 'thur') {
+    console.log('class day');
+  } else if (day === 'mon' || day === 'wed' || day === 'fri') {
+    console.log('boring day');
+  } else {
+    console.log('weekend');
+  }
 }
