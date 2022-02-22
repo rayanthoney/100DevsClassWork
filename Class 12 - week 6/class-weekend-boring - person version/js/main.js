55d0629e589f08e42c9779623872e777ca8b290c
Ryan <ryanmahara@gmail.com>
2022-02-22 13:36:25 -0600

    remove class JS

--- Class 12 - week 6/class-weekend-boring - person version/js/main.js
+++ Class 12 - week 6/class-weekend-boring - person version/js/main.js
@@ -1,14 +1,3 @@
-document.querySelector('#check').addEventListener('click', check);
-
-function check() {
-  const day = document.querySelector('#day').value.toLowerCase();
-
-  //Conditionals go here
-  if (day === 'tue' || day === 'thur') {
-    console.log('class day');
-  } else if (day === 'mon' || day === 'wed' || day === 'fri') {
-    console.log('boring day');
-  } else {
-    console.log('weekend');
-  }
-}
+// on tue/thur = class day
+// on mon/wed/fri = boring day
+// on sat/sun = weekend
