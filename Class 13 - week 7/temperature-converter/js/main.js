b6cef606ab69c6b11af11fe19cb2552c541eddef
Ryan <ryanmahara@gmail.com>
2022-02-23 10:48:55 -0600

    class 13 in class code

--- Class 13 - week 7/temperature-converter/js/main.js
+++ Class 13 - week 7/temperature-converter/js/main.js
@@ -1,1 +1,12 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+
+// get input
+// find if its f or c
+// convert to opposite
+
+const temp = document.querySelector('#temp');
+
+temp.addEventListener('change', (e) => {
+  const value = e.target.value;
+  console.log(Number(value) * (9 / 5) + 32);
+});
