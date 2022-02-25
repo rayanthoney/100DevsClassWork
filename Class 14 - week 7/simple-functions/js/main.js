8b45bc2e5f82facd0f7433377767bd983b629f6d
Ryan <ryanmahara@gmail.com>
2022-02-25 13:51:24 -0600

    class work for class 13

--- Class 14 - week 7/simple-functions/js/main.js
+++ Class 14 - week 7/simple-functions/js/main.js
@@ -1,5 +1,11 @@
 //create a function that adds two numbers and alerts the sum
+// const add = (a, b) => a + b;
+
+const add = (values) => values.reduce((acc, cur) => (acc += cur), 0);
+console.log(add([1, 2, 3, 4]));
 
 //create a function that multiplys three numbers and console logs the product
+const multiply = (a, b, c) => console.log(a * b * c);
 
 //create a function that divides two numbers and returns the ???
+const divide = (a, b) => a / b;
