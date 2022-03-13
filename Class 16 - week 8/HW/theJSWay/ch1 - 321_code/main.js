816f7df53fe0649a9710264b88615dd14d73f9d3
Ryan <ryanmahara@gmail.com>
2022-03-13 17:38:19 -0500

    TheJSWay - ch1 - 321code challenge completed

---
+++ Class 16 - week 8/HW/theJSWay/ch1 - 321_code/main.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Presentation
+
+console.log('Ryan');
+console.log('24');
+
+// Minimalistic calculator
+
+console.log(6 + 3);
+console.log(6 - 3);
+console.log(6 * 3);
+console.log(6 / 3);
+
+// Values prediction
+
+console.log(4 + 5); // 9
+console.log('4 + 5'); // "4 + 5"
+console.log('4' + '5'); // 45
