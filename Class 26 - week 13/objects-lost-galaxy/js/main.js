2fc3e706846b36f84e5cca24838b82f8e5acf727
Ryan <ryanmahara@gmail.com>
2022-04-07 18:57:59 -0500

    complete in class exercise 1

--- Class 26 - week 13/objects-lost-galaxy/js/main.js
+++ Class 26 - week 13/objects-lost-galaxy/js/main.js
@@ -1,1 +1,27 @@
-//Create a dog object that has four properties and three methods
+//Create a pizza object that has four properties and three methods
+const pizza = new Object();
+
+pizza.type = 'supreme';
+pizza.cookTime = '15min';
+pizza.temp = 450;
+pizza.numOfSlices = 8;
+
+pizza.instructions = function () {
+  return `The pizza is going in the oven for ${this.cookTime} at ${this.temp} deg's`;
+};
+
+pizza.handOut = function () {
+  for (let i = 0; i <= this.numOfSlices; i++) {
+    console.log(`hand out pizza slice ${i}`);
+  }
+};
+
+pizza.whatType = function () {
+  return `we eating ${this.type} pizza today`;
+};
+
+console.log(pizza.instructions());
+
+console.log(pizza.whatType());
+
+pizza.handOut();
