f38c616296effbb76199e54159db3713d5db5f42
Ryan <ryanmahara@gmail.com>
2022-04-07 19:56:55 -0500

    complete class 26 in class exercise 2

--- Class 26 - week 13/objects-constructor-fun/js/main.js
+++ Class 26 - week 13/objects-constructor-fun/js/main.js
@@ -1,1 +1,29 @@
 //Create a constructor with 4 properties and 3 methods
+
+function Pizza(type, cookTime, temp, numOfSlices) {
+  this.type = type;
+  this.cookTime = cookTime;
+  this.temp = temp;
+  this.numOfSlices = numOfSlices;
+
+  this.instructions = function () {
+    return `The pizza is going in the oven for ${this.cookTime} at ${this.temp} deg's`;
+  };
+
+  this.handOut = function () {
+    for (let i = 0; i <= this.numOfSlices; i++) {
+      console.log(`hand out pizza slice ${i}`);
+    }
+  };
+
+  this.whatType = function () {
+    return `we eating ${this.type} pizza today`;
+  };
+}
+
+const pizza = new Pizza('supreme', '15min', 450, 8);
+console.log(pizza.instructions());
+
+console.log(pizza.whatType());
+
+pizza.handOut();
