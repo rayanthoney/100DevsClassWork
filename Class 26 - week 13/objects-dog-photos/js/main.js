387767bda460752dc459bc48b9b6659fc3d65c06
Ryan <ryanmahara@gmail.com>
2022-04-12 17:37:13 -0500

    100devs class 27 folder structure

--- Class 26 - week 13/objects-dog-photos/js/main.js
+++ Class 26 - week 13/objects-dog-photos/js/main.js
@@ -1,1 +1,13 @@
 //Get a dog photo from the dog.ceo api and place the photo in the DOM
+
+const imgEl = document.querySelector('img');
+
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    console.log(data);
+    imgEl.src = data.message;
+  })
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
--- Class 26 - week 13/objects-shots/js/main.js
+++ Class 26 - week 13/objects-shots/js/main.js
@@ -1,1 +1,20 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+
+document.querySelector('button').addEventListener('click', (e) => {
+  let inputValue = document.querySelector('input').value;
+
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`;
+
+  fetch(url)
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+
+      document.querySelector('h2').textContent = data.drinks[0].strDrink;
+      document.querySelector('img').src = data.drinks[0].strDrinkThumb;
+      document.querySelector('h3').textContent = data.drinks[0].strInstructions;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+});
