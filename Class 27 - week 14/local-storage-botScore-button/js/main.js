20b3d015d602df29c53ab38ab76e481363db0b0d
Ryan <ryanmahara@gmail.com>
2022-04-13 16:16:09 -0500

    class 27 in class work

--- Class 27 - week 14/local-storage-botScore-button/js/main.js
+++ Class 27 - week 14/local-storage-botScore-button/js/main.js
@@ -1,3 +1,15 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+//Create a button that adds 1 to a botScore stored in localStorage
 
+const outputEl = document.querySelector('h2');
 
+if (localStorage.getItem('score') !== null) {
+  outputEl.textContent = localStorage.getItem('score');
+}
+
+document.querySelector('button').addEventListener('click', (e) => {
+  const curNumber = Number(outputEl.textContent);
+
+  outputEl.textContent = curNumber + 1;
+
+  localStorage.setItem('score', outputEl.textContent);
+});
--- Class 27 - week 14/objects-netflix-tv-shows/js/main.js
+++ Class 27 - week 14/objects-netflix-tv-shows/js/main.js
@@ -1,3 +1,26 @@
 //Create a Netflix TV Show class with a constructor that makes Netflix TV Shows with 4 properties and 3 methods
 
+class NetflixShow {
+  constructor(name, eps, duration, releaseDate) {
+    this.name = name;
+    this.eps = eps;
+    this.duration = duration;
+    this.releaseDate = releaseDate;
+  }
 
+  getTitle() {
+    return `${this.name}`;
+  }
+
+  totalEpisodes() {
+    return this.eps;
+  }
+
+  whenDoesItReleaseDate() {
+    return this.releaseDate;
+  }
+}
+
+const test = new NetflixShow('anime', 12, '24mins', '04/13/2022');
+
+console.log(test);
