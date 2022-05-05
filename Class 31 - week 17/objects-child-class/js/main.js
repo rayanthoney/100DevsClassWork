ecfdf32b1a23fac321a896f4f4c2382514b15d59
Ryan <ryanmahara@gmail.com>
2022-05-05 17:43:02 -0500

    class 31 in class work

--- Class 31 - week 17/objects-child-class/js/main.js
+++ Class 31 - week 17/objects-child-class/js/main.js
@@ -1,2 +1,18 @@
-//Create an a class and extend it - Can be anything you would like it to be! 
+//Create an a class and extend it - Can be anything you would like it to be!
 
+class Human {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Male extends Human {
+  constructor(name, age, hasCar) {
+    super(name, age);
+    this.hasCar = hasCar;
+  }
+}
+
+const boy = new Male('ryan', 24, true);
+console.log(boy);
