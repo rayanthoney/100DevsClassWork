e38961580753b49bd6d7c6210cdb5d1ef0dda526
Ryan <ryanmahara@gmail.com>
2022-05-13 11:06:54 -0500

    add completed ch5 exercise 4 for eloquent JS class 29 HW

--- Class 29 - week 16/HW/eloquentJS/ch5/script-exercises.js
+++ Class 29 - week 16/HW/eloquentJS/ch5/script-exercises.js
@@ -80,3 +80,84 @@ console.log(every2([], (n) => n < 10));
 // → true
 
 // 4. Dominant writing direction
+
+/*
+Write a function that computes the dominant writing direction in a string of text. Remember that each script object has a direction property that can be "ltr" (left to right), "rtl" (right to left), or "ttb" (top to bottom).
+
+The dominant direction is the direction of a majority of the characters that have a script associated with them. The characterScript and countBy functions defined earlier in the chapter are probably useful here.
+
+
+*/
+
+console.log(`
+
+
+Exercise 4 start
+
+
+`);
+
+import SCRIPTS from './dataSet.js';
+// console.log(SCRIPTS);
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (
+      // [125184, 125259]
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+}
+
+// console.log(characterScript(121));
+// → {name: "Latin", …}
+// console.log(characterScript('!'.charCodeAt()));
+// console.log(characterScript(33));
+// console.log(characterScript(121));
+
+// function countBy(items, groupName) {
+//   let counts = [];
+//   for (let item of items) {
+//     let name = groupName(item);
+//     let known = counts.findIndex((c) => c.name == name);
+//     if (known == -1) {
+//       counts.push({ name, count: 1 });
+//     } else {
+//       counts[known].count++;
+//     }
+//   }
+//   return counts;
+// }
+
+// console.log(countBy([1, 2, 3, 4, 5], (n) => n > 2));
+// → [{name: false, count: 2}, {name: true, count: 3}]
+
+function dominantDirection(text) {
+  let totalObj = text.split('').reduce(
+    (obj, char) => {
+      let result = characterScript(char.charCodeAt());
+      obj[result?.direction ?? 'none'] += 1;
+      return obj;
+    },
+    { ltr: 0, rtl: 0, ttb: 0, none: 0 }
+  );
+
+  let highest = [0, ''];
+  for (let prop in totalObj) {
+    if (totalObj[prop] > highest[0]) {
+      highest[0] = totalObj[prop];
+      highest[1] = prop;
+    }
+  }
+  return highest[1];
+}
+
+console.log(dominantDirection('Hello!'));
+// → ltr
+
+console.log(dominantDirection('Hey, مساء الخير'));
+// → rtl
