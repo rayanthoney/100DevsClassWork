116a0d189f8c55b09957804b4df297dc9411dabc
Ryan <ryanmahara@gmail.com>
2022-05-26 14:57:33 -0500

    100devs class 36 thursday HW - make server.js dryer

--- Class 36 - week 20/node-backend-simple-json/server.js
+++ Class 36 - week 20/node-backend-simple-json/server.js
@@ -1,73 +1,81 @@
 const http = require('http');
-const fs = require('fs')
+const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
-const figlet = require('figlet')
+// figlet creates ascii art for words
+const figlet = require('figlet');
 
+const contentType = {
+  html: 'text/html',
+  json: 'application/json',
+  js: 'text/javascript',
+  css: 'text/css',
+};
+
+const allHTMLFiles = {
+  '/': 'index.html',
+  '/otherpage': 'otherpage.html',
+  '/otherotherpage': 'otherotherpage.html',
+};
+
+// utility functions
+function hostReadFile(file, type = html, res) {
+  console.log('inside hostReadFile');
+  fs.readFile(file, function (err, data) {
+    res.writeHead(200, { 'Content-Type': `${contentType[type]}` });
+    res.write(data);
+    res.end();
+  });
+}
+
+function handleAPICall(params, res) {
+  const data = {
+    leon: {
+      name: 'leon',
+      status: 'Boss Man',
+      currentOccupation: 'Baller',
+    },
+    unknown: {
+      name: 'unknown',
+      status: 'unknown',
+      currentOccupation: 'unknown',
+    },
+  };
+
+  if ('student' in params) {
+    let student = params['student'] === 'leon' ? 'leon' : 'unknown';
+    console.log(student);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(data[student]));
+  } //student if
+}
+
+// returns a new instance of http.server object
 const server = http.createServer((req, res) => {
+  // url.parse is legacy
   const page = url.parse(req.url).pathname;
+  // new URL() is current, using
+  // const page = new URL(req.url).pathname;
+  console.log(`page is equal too :   ${page}`);
+  // front end side a fetch is being used with fetch(`/api?student=${userName}`) coming from the input
   const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/otherotherpage') {
-    fs.readFile('otherotherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
-  else if (page == '/api') {
-    if('student' in params){
-      if(params['student']== 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "leon",
-          status: "Boss Man",
-          currentOccupation: "Baller"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
-      else if(params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: "unknown",
-          status: "unknown",
-          currentOccupation: "unknown"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
-  }//else if
-  else if (page == '/css/style.css'){
-    fs.readFile('css/style.css', function(err, data) {
-      res.write(data);
-      res.end();
-    });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
-  }else{
-    figlet('404!!', function(err, data) {
+
+  if (page in allHTMLFiles) {
+    hostReadFile(allHTMLFiles[page], 'html', res);
+  } else if (page == '/api') {
+    handleAPICall(params, res);
+  } //else if
+  else if (page == '/css/style.css') {
+    hostReadFile('css/style.css', 'css', res);
+  } else if (page == '/js/main.js') {
+    hostReadFile('js/main.js', 'js', res);
+  } else {
+    // error handling if people look up a page that doesn't exist
+    figlet('404!!', function (err, data) {
       if (err) {
-          console.log('Something went wrong...');
-          console.dir(err);
-          return;
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
       }
       res.write(data);
       res.end();
@@ -75,4 +83,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
+// Starts the HTTP server listening for connections. This method is identical to server.listen() from net.Server.
 server.listen(8000);
+// console.log(server);
+
+module.exports = server;
